ad36c8162cd01862b8c3f0825eafc6c88fb28905
yoel-z <yoel.zerbib@suridata.ai>
2022-12-06 20:02:53 +0200

    updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
   
 @app.post('/generate_messages')
 async def run_app(request: Request):
-    return {"messages": "hello"}
+    return {"messages": "hello my man !"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=6262)
